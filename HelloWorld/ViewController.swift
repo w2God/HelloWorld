596c84bcdc03bc71b7631313651cb393cad9fe1c
snam <nam.s@chocomart.kz>
2022-10-05 15:46:54 +0600

    add action

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorldLabel: UILabel!
+    
+    @IBOutlet var toggleButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        helloWorldLabel.isHidden = true
+        toggleButton.layer.cornerRadius = 10
+
     }
 
-
+    @IBAction func makeButtonAction() {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            toggleButton.setTitle("Hide Text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            toggleButton.setTitle("Show Text", for: .normal)
+        }
+        
+    }
+    
 }
 
